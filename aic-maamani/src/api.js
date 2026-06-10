499aba8c014054294b4d5708d7dba53eea5558d7
Your Name <you@example.com>
2026-06-10 10:02:25 +0300

    commit

--- aic-maamani/src/api.js
+++ aic-maamani/src/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = process.env.REACT_APP_API_BASE_URL || window.location.origin;
+export const API_BASE = (process.env.REACT_APP_API_BASE_URL || "").replace(/\/$/, "") || window.location.origin;
 
 export async function fetchJson(path, options = {}) {
   const response = await fetch(`${API_BASE}${path}`, {
